2cf09e338a66a12469bfbd95714bd57079f73f5e
artvsb <artvsb@outlook.com>
2026-03-19 10:41:38 -0300

    conexão com BD ^^ ajustes em aluno controller e repository

---
+++ src/main/java/br/com/alunoonline/api/controller/AlunoController.java
@@ -0,0 +1,23 @@
+package br.com.alunoonline.api.controller;
+
+import br.com.alunoonline.api.model.Aluno;
+import br.com.alunoonline.api.service.AlunoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController // controller que opera em arquitetura Rest
+@RequestMapping("/alunos") /* mapeamento de requisição -> toda requisição HTTP que chegar em /alunos vai bater primeiro no AlunoController
+"/alunos" é um endpoint */
+public class AlunoController {
+
+    @Autowired
+    AlunoService alunoService; // injeção do service no controller
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED) // o status da resposta que deve ser dada à request do POST
+    public void criarAluno(@RequestBody Aluno aluno) {
+        // a anotação @RequestBody converte o corpo do request de Json para Java
+        alunoService.criarAluno(aluno);
+    }
+}
--- src/main/java/br/com/alunoonline/api/repository/AlunoRepository.java
+++ src/main/java/br/com/alunoonline/api/repository/AlunoRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AlunoRepository extends JpaRepository<Aluno, Long> {
-    // 'extends JpaRepository' torna possível o CRUD do repository no Banco de Dados
-	// extends JpaRepository <nome do model, tipo da chave primária desse model>
+    /* 'extends JpaRepository' torna possível o CRUD do repository no Banco de Dados
+	 extends JpaRepository <nome do model, tipo da chave primária desse model>
+     alunoRepository só aceita a entidade Aluno como entrada */
 }
